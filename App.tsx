79b2327d1b720f0d2216b05aace5f196a93c6a56
artemijfridriksen <freshart666@gmail.com>
2024-02-17 17:34:28 -0500

    BUG PIZDA

--- App.tsx
+++ App.tsx
@@ -1,23 +1,22 @@
-import React, {useState} from 'react';
+import React from 'react';
 import CocktailsAlcForm from './src/contents/CocktailsAlc';
 import NonCocktailsAlcForm from './src/contents/NonCocktailsAlc';
 import ProfileForm from './src/contents/Profile';
+import LoginForm from './src/contents/auth/Login';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { ActivityIndicator, View } from 'react-native'; 
+import { registerRootComponent } from 'expo';
 
 const Tab = createBottomTabNavigator();
-
-function App() {
-
-  const[loading, setLoading] = useState(true)
-
+const App = () => {
+  
   return (
     <NavigationContainer>
       <Tab.Navigator>
         <Tab.Screen name={'Alcohole'} component={CocktailsAlcForm} />
         <Tab.Screen name={'Non alcohole'} component={NonCocktailsAlcForm} />
         <Tab.Screen name={'Profile'} component={ProfileForm}/>
+        <Tab.Screen name={'Login'} component={LoginForm}/>
       </Tab.Navigator>
     </NavigationContainer>
   );
--- src/contents/auth/Login.tsx
+++ src/contents/auth/Login.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import {Button, SafeAreaView} from 'react-native';
+import {Button, SafeAreaView, Text} from 'react-native';
 import Auth0 from 'react-native-auth0';
 import auth0Credentials from './auth0-credentials';
 
 const auth0 = new Auth0(auth0Credentials);
+const [email, onEmailChanged] = React.useState('Email');
+const [password, onPasswordChanged] = React.useState('Password');
 
-function App() {
+function LoginForm() {
   function login() {
     auth0.webAuth
       .authorize({scope: 'openid profile email'})
@@ -14,12 +16,19 @@ function App() {
       })
       .catch(error => console.log(error));
   };
-
   return (
     <SafeAreaView>
+
+     
+
       <Button title="Log In" onPress={login} />
+
+      <Text style={{textAlign: 'center'}}>OR</Text>
+
+      
+
     </SafeAreaView>
   );
 };
 
-export default App;
+export default LoginForm;
